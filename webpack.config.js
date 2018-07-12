1d6ba263a058839aed959044ee418c93e6d1233a
Isaac <icruz@rappi.com>
2018-07-11 21:26:13 -0500

    gitignore added

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+    template: __dirname + '/src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
+
+module.exports = {
+    entry: __dirname + '/src/index.js',
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        },{
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+        }]
+    },
+    output: {
+        filename: 'transformed.js',
+        path: __dirname + '/build'
+    },
+    plugins: [HTMLWebpackPluginConfig]
+};
